7c8116c9abbd275330a194d593ad0109cba2ca1b
bniepce <brad.niepce@kaptios.com>
2024-11-25 15:53:41 +0100

    edit augmentations

--- src/utils/augmentations.py
+++ src/utils/augmentations.py
@@ -13,8 +13,14 @@ def make_augmentation(data_dir, num_augmentations: int = 3):
     augmentation_pipeline = A.Compose([
         A.HorizontalFlip(p=0.5),
         A.VerticalFlip(p=0.5),
-        A.Rotate(limit=45, always_apply=True,
-                 border_mode=cv2.BORDER_CONSTANT, p=1.0),
+        A.Rotate(limit=45, border_mode=cv2.BORDER_CONSTANT, p=0.3),
+        A.ElasticTransform(alpha=1, sigma=50, p=0.3),
+        A.RandomBrightnessContrast(
+            brightness_limit=0.2, contrast_limit=0.2, p=0.5),
+        A.GaussNoise(var_limit=(10.0, 50.0), p=0.3),
+        A.GaussianBlur(blur_limit=(3, 7), p=0.3),
+        A.RandomGamma(gamma_limit=(80, 120), p=0.4),
+        A.RandomResizedCrop(height=224, width=224, scale=(0.9, 1.0), p=0.5),
     ])
 
     label_folders = glob(os.path.join(data_dir, '*'))
